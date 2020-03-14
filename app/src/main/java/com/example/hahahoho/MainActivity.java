d55c0c81c835d44b7cd4f779262bbd19685b9e79
dongsik <ehdtlr9376@naver.com>
2020-03-14 16:38:11 +0900

    Complete Toolbar appBar

--- app/src/main/java/com/example/hahahoho/MainActivity.java
+++ app/src/main/java/com/example/hahahoho/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.hahahoho;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
@@ -20,11 +24,16 @@ public class MainActivity extends AppCompatActivity {
     private fragment_menu2 menu2Fragment = new fragment_menu2();
     private fragment_menu3 menu3Fragment = new fragment_menu3();
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // 툴바
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
+        // Bottom nav
         BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
         // 첫화면 지정
         FragmentTransaction transaction = fragmentManager.beginTransaction();
@@ -51,5 +60,11 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+    // XML에 정의한 appbar menu가 나타나도록
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar_menu, menu) ;
+        return true ;
     }
 }
