654858f5a9e1e33b957e3de4955deae8e49add8e
dongsik <ehdtlr9376@naver.com>
2020-03-15 18:55:43 +0900

    Complete Read json in ListView

--- app/src/main/java/com/example/hahahoho/fragment_menu1.java
+++ app/src/main/java/com/example/hahahoho/fragment_menu1.java
@@ -1,5 +1,6 @@
 package com.example.hahahoho;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 
 import androidx.core.content.ContextCompat;
@@ -7,17 +8,63 @@ import androidx.fragment.app.Fragment;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.logging.Logger;
 
 
 public class fragment_menu1 extends Fragment {
+    InputStream source = null;
+    String str = null;
+    ArrayList<JSONObject> list = new ArrayList<JSONObject>();
+    JSONObject arr= null;
+    int cnt = 0;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // assets 장치검색
+        AssetManager assetManager = getResources().getAssets();
+
+        try {
+            // assets 폴더에서 해당 json을 가저옴
+            source = assetManager.open("hobby.json");
+            int i;
+            StringBuffer buffer = new StringBuffer(); byte[] b = new byte[4096];
+            while( (i = source.read(b)) != -1){
+                buffer.append(new String(b, 0, i));
+            }
+            str = buffer.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            arr = new JSONObject(str);
+            JSONArray jsonArray = arr.getJSONArray("Hobbies");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                list.add(jsonArray.getJSONObject(i));
+                cnt ++;
+            }
+            Log.d("test", "-----------------------------");
+            Log.d("test", String.valueOf(list));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Nullable
@@ -27,16 +74,29 @@ public class fragment_menu1 extends Fragment {
         View view = inflater.inflate(R.layout.fragment_menu1, container, false);
         ListView listview = view.findViewById(R.id.listView);
         MyAdapter listViewAdatper = new MyAdapter();
-        listViewAdatper.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.logo), "11", "11");
-        listViewAdatper.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.logo), "22", "22");
-        listViewAdatper.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.logo), "33", "33");
-        listViewAdatper.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.logo), "44", "44");
+
+        String img = null;
+        for(int i=0; i<cnt; i++){
+            try {
+                Class<R.drawable> drawable = R.drawable.class;
+                img = list.get(i).getString("img");
+                Field field = drawable.getField( img );
+                int r = field.getInt(null);
+                listViewAdatper.addItem(ContextCompat.getDrawable(getActivity(), r), list.get(i).getString("title"), list.get(i).getString("subTitle") );
+            } catch (JSONException | NoSuchFieldException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
 
         listview.setAdapter(listViewAdatper);
-
+        cnt = 0;
         return view;
     }
 
 }
 
-
+class Hobby {
+    String title;
+    String subTitle;
+    String img;
+}
